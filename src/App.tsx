f26722b75083388366648cdc9249dcdc79e915c9
launchpad5682 <launchpad5682@gmail.com>
2023-01-14 18:14:45 +0530

    chore: initial setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+/** @jsxImportSource @emotion/react */
+import { useEffect, useState } from 'react';
 import './App.css';
+import * as classNames from './style';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [first, setFirst] = useState(false);
+
+	useEffect(() => {
+		console.log('on initial mount', first);
+		setFirst(true);
+	}, []);
+
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p css={classNames.paraContainer}>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
