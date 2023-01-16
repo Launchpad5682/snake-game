95239d06315ef7a9a0fe01a526e598253517bc4e
launchpad5682 <launchpad5682@gmail.com>
2023-01-16 19:41:22 +0530

    refactor: data and types moved to seperate files in data-context

---
+++ src/contexts/data-context/data/index.ts
@@ -0,0 +1,15 @@
+import { Snake } from '../types';
+
+const initialSnakeValue: Snake = [
+	{ x: 12, y: 10, direction: 'left' },
+	{ x: 13, y: 10, direction: 'left' },
+	{ x: 14, y: 10, direction: 'left' },
+	{ x: 15, y: 10, direction: 'left' },
+	{ x: 16, y: 10, direction: 'left' },
+	{ x: 17, y: 10, direction: 'left' },
+	{ x: 18, y: 10, direction: 'left' },
+	{ x: 19, y: 10, direction: 'left' },
+	{ x: 20, y: 10, direction: 'left' },
+];
+
+export { initialSnakeValue };
---
+++ src/contexts/data-context/types/index.ts
@@ -0,0 +1,32 @@
+type DirectionType = 'up' | 'down' | 'left' | 'right';
+type CellType = {
+	x: number;
+	y: number;
+	snakeCell: boolean;
+	snakeHead: boolean;
+	foodCell: boolean;
+};
+type GridType = Array<Array<CellType>>;
+type Position = { x: number; y: number };
+type SnakePosition = {
+	x: number;
+	y: number;
+	direction: DirectionType;
+};
+type Snake = Array<SnakePosition>;
+
+export interface DataContextInterface {
+	direction: DirectionType;
+	grid: GridType;
+	keyPressHandler: () => any;
+	score: number;
+}
+
+export type {
+	DirectionType,
+	CellType,
+	GridType,
+	Position,
+	SnakePosition,
+	Snake,
+};
