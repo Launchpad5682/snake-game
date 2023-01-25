22b3009d8ed64b814da63aec0a7779028f5ffa17
launchpad5682 <launchpad5682@gmail.com>
2023-01-25 18:57:35 +0530

    refactor: keydown listener moved to document

--- src/components/playground/index.tsx
+++ src/components/playground/index.tsx
@@ -1,5 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { useTheme } from '@emotion/react';
+import { useEffect } from 'react';
 import { useDataContext } from '../../contexts/data-context';
 import { DataContextInterface } from '../../contexts/data-context/types';
 import { ColorsType } from '../../contexts/theme-context';
@@ -8,28 +9,43 @@ import * as classNames from './style';
 
 function Playground() {
 	const theme = useTheme() as ColorsType;
-	const { grid, keyPressHandler } = useDataContext() as DataContextInterface;
+	const { grid, keyPressHandler, end, restartGame } =
+		useDataContext() as DataContextInterface;
+
+	useEffect(() => {
+		document.addEventListener('keydown', keyPressHandler);
+		return () => document.removeEventListener('keydown', keyPressHandler);
+	}, [keyPressHandler]);
 
 	return (
-		// eslint-disable-next-line jsx-a11y/no-static-element-interactions
-		<div
-			css={classNames.playgroundContainer}
-			// eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-			tabIndex={-1}
-			onKeyDown={keyPressHandler}
-		>
+		<div css={classNames.playgroundContainer}>
 			<div css={classNames.playground(theme)}>
 				{grid.map((row, rowIdx) =>
-					row.map(({ snakeCell, foodCell }, colIdx) => (
+					row.map(({ snakeCell, foodCell, snakeHead }, colIdx) => (
 						<GridElement
 							// eslint-disable-next-line react/no-array-index-key
 							key={`${rowIdx}_${colIdx}`}
 							snakeCell={snakeCell}
 							foodCell={foodCell}
+							snakeHead={snakeHead}
 						/>
 					)),
 				)}
 			</div>
+			<div css={classNames.buttonContainer}>
+				{end ? (
+					<button
+						type="button"
+						onClick={restartGame}
+						css={classNames.restartButton(theme)}
+					>
+						Reset Game
+					</button>
+				) : null}
+			</div>
+			<span css={{ fontSize: '2rem' }}>
+				Use spacebar to play/pause game, and arrow keys to move snake.
+			</span>
 		</div>
 	);
 }
